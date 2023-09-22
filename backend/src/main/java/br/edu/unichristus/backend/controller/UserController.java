53cbf5e8a96f0611af8430482f46c63fc7fb1621
Adriano Lima <jink_adriano@hotmail.com>
2023-09-22 20:34:14 -0300

    aula 18-09
    aula 18-09

--- backend/src/main/java/br/edu/unichristus/backend/controller/UserController.java
+++ backend/src/main/java/br/edu/unichristus/backend/controller/UserController.java
@@ -3,8 +3,11 @@ package br.edu.unichristus.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +24,22 @@ public class UserController {
 
 	@PostMapping
 	public User create(@RequestBody User user) {
-		return service.create(user);
+		return service.save(user);
+	}
+	
+	@PutMapping
+	public User update(@RequestBody User user) {
+		return service.save(user);
+	}
+	
+	@GetMapping("/{id}")
+	public User findById(@PathVariable("id") Long id) {
+		return service.findById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") Long id) {
+		service.delete(id);
 	}
 	
 	@GetMapping
--- backend/src/main/java/br/edu/unichristus/backend/service/UserService.java
+++ backend/src/main/java/br/edu/unichristus/backend/service/UserService.java
@@ -13,12 +13,40 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	public User create(User user) {
+	public User save(User user) {
 		if(user.getName().length() > 100) {
 			System.out.println("O nome excede a quantidade de caracteres");
 		}
 		
+		if(user.getId() != null) {
+			var userFind = this.findById(user.getId());
+			if(userFind == null) {
+				return null;
+			}
+		}
+		
 		return repository.save(user);
+	}
+	
+	public void delete(Long id) {
+		var userFind = this.findById(id);
+		if(userFind == null) {
+			return;
+		}
+		
+		repository.deleteById(id);
+	}
+	
+	
+	
+	public User findById(Long id) {
+		var user = repository.findById(id);
+		if(user == null || user.isEmpty()) {
+			System.out.println("A ID informada não existe");
+			return null;
+		}
+		
+		return user.get();
 	}
 	
 	public List<User> findAll(){
