56979e90a4503869645363b904369b0df53c9e66
Adriano Lima <jink_adriano@hotmail.com>
2023-11-13 21:50:46 -0300

    projeto front 13/11

---
+++ frontend/front/src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+import logoImage from '../../assets/logo.png';
+import padLock from '../../assets/padlock.png';
+
+export default function Login(){
+    return(
+        
+    );
+}
