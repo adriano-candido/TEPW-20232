7f914e84dd29460588908779e4fc500f0ccf8e05
Adriano Lima <jink_adriano@hotmail.com>
2023-12-04 21:53:58 -0300

    aula 04/12

--- frontend/front/src/pages/User/index.js
+++ frontend/front/src/pages/User/index.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
 import { Link } from 'react-router-dom';
 import {FiPower, FiEdit, FiTrash2 } from 'react-icons/fi'
 
 import './styles.css';
 import logoImage from '../../assets/logo.png';
 
+import api from '../../services/api';
+
 export default function User(){
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        api.get(`api/v1/user`).then(response => {
+            setUsers(response.data);
+         } )
+     } )
+
+     async function deleteUser(id){
+        try{
+            await api.delete(`api/v1/user/${id}`)
+        }catch(err){
+            alert("Falha ao deletar usuário! Tente novamente.");
+        }
+     }
+
     return (
         <div className="user-container">
             <header>
@@ -19,70 +38,26 @@ export default function User(){
 
             <h1>Usuários Cadastrados</h1>
             <ul>
+                {users.map(user => 
                 <li>
                     <strong>Nome</strong>
-                    <p>João Lima</p>
+                    <p>{user.nome}</p>
                     <strong>E-mail</strong>
-                    <p>joaozinho@gmail.com</p>
+                    <p>{user.email}</p>
                     <strong>Usuário</strong>
-                    <p>jao123
+                    <p>{user.login}</p>
 
                         <button type="button">
                             <FiEdit size={20} color="#251FC5"/>
                         </button>
-                        <button type="button">
+                        <button 
+                        onClick={ () => deleteUser(user.id)}
+                        type="button">
                             <FiTrash2 size={20} color="#251FC5"/>
                         </button>
-                    </p>
+                    
                 </li>
-                <li>
-                    <strong>Nome</strong>
-                    <p>João Lima</p>
-                    <strong>E-mail</strong>
-                    <p>joaozinho@gmail.com</p>
-                    <strong>Usuário</strong>
-                    <p>jao123
-
-                        <button type="button">
-                            <FiEdit size={20} color="#251FC5"/>
-                        </button>
-                        <button type="button">
-                            <FiTrash2 size={20} color="#251FC5"/>
-                        </button>
-                    </p>
-                </li>
-                <li>
-                    <strong>Nome</strong>
-                    <p>João Lima</p>
-                    <strong>E-mail</strong>
-                    <p>joaozinho@gmail.com</p>
-                    <strong>Usuário</strong>
-                    <p>jao123
-
-                        <button type="button">
-                            <FiEdit size={20} color="#251FC5"/>
-                        </button>
-                        <button type="button">
-                            <FiTrash2 size={20} color="#251FC5"/>
-                        </button>
-                    </p>
-                </li>
-                <li>
-                    <strong>Nome</strong>
-                    <p>João Lima</p>
-                    <strong>E-mail</strong>
-                    <p>joaozinho@gmail.com</p>
-                    <strong>Usuário</strong>
-                    <p>jao123
-
-                        <button type="button">
-                            <FiEdit size={20} color="#251FC5"/>
-                        </button>
-                        <button type="button">
-                            <FiTrash2 size={20} color="#251FC5"/>
-                        </button>
-                    </p>
-                </li>
+                )}
             </ul>
         </div>
     );
